3c26909f8e2c2a7c8b7d4278280f00f53e00670b
Eric Kivel <ericvkivel1@gmail.com>
2022-06-25 22:30:32 -0300

    Feature: Command RuMenu

---
+++ cassiohcore/Commands/RuMenu.ts
@@ -0,0 +1,154 @@
+const puppeteer = require("puppeteer");
+
+import { CommandModel } from "../Modal/CommandModel";
+import { params_to_command } from "../Modal/keyTreatment";
+import { SendReplyCommand } from "./SendReply";
+
+export class RuMenuCommand extends CommandModel {
+  protected _active: boolean = true;
+  protected _hidden: boolean = false;
+  protected _name: string = "/ru";
+  protected _description: string = "Mostra o cardápio do RU";
+  protected _key: string = "ru";
+  protected _access_level: number = 3;
+
+  protected async execute_command(params?: params_to_command): Promise<void> {
+    console.log("Rodando Cardápio do RU!");
+    try {
+      const getMenu = async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://pra.ufpr.br/ru/ru-centro-politecnico/", {
+          waitUntil: "networkidle2",
+        });
+
+        await page.waitForSelector("tbody");
+        await page.waitForSelector("p > strong");
+
+        const response = await page.evaluate(() => {
+          const breakfastTitle: any = document.querySelectorAll(
+            "tbody > tr:nth-child(1)"
+          );
+          const lunchTitle: any = document.querySelectorAll(
+            "tbody > tr:nth-child(3)"
+          );
+          const dinnerTitle: any = document.querySelectorAll(
+            "tbody > tr:nth-child(5)"
+          );
+
+          const breakfastHTML: any = document.querySelectorAll(
+            "tbody > tr:nth-child(2)"
+          );
+          const lunchHTML: any = document.querySelectorAll(
+            "tbody > tr:nth-child(4)"
+          );
+          const dinnerHTML: any = document.querySelectorAll(
+            "tbody > tr:nth-child(6)"
+          );
+          let daysHTML: any = document.querySelectorAll("p > strong");
+
+          const numberOfTables: any = document.querySelectorAll("table").length;
+
+          let days = [""];
+
+          if (numberOfTables === 1) {
+            // Checks if the first day has a table
+            if (
+              document.querySelector("p + figure")?.children.toString() ===
+              "[object HTMLTableElement]"
+            ) {
+              days = [daysHTML[0].innerText.toString()];
+            } else {
+              days = [daysHTML[1].innerText.toString()];
+            }
+          } else {
+            days = [
+              daysHTML[0].innerText.toString(),
+              daysHTML[1].innerText.toString(),
+            ];
+          }
+
+          const titles = days.map((day: string, index: number) => {
+            return {
+              breakfast: breakfastTitle[index].innerText.toString(),
+              lunch: lunchTitle[index].innerText.toString(),
+              dinner: dinnerTitle[index].innerText.toString(),
+            };
+          });
+
+          const menus = days.map((day: string, index: number) => {
+            return {
+              breakfast: breakfastHTML[index].innerText.toString(),
+              lunch: lunchHTML[index].innerText.toString(),
+              dinner: dinnerHTML[index].innerText.toString(),
+            };
+          });
+
+          let menuResponse = [];
+
+          menuResponse = days.map((day: string, index: number) => {
+            return {
+              weekDay: days[index].toString(),
+              breakfast: {
+                title: titles[index].breakfast.toString(),
+                menu: menus[index].breakfast.toString(),
+              },
+              lunch: {
+                title: titles[index].lunch.toString(),
+                menu: menus[index].lunch.toString(),
+              },
+              dinner: {
+                title: titles[index].dinner.toString(),
+                menu: menus[index].dinner.toString(),
+              },
+            };
+          });
+
+          return menuResponse;
+        });
+
+        await browser.close();
+
+        return response;
+      };
+
+      const ruMenu = await getMenu();
+
+      const parseMenu = (menu: string): string => {
+        return menu
+          .split("\n")
+          .map((meal: string, index: number) => {
+            if (index === 0) {
+              return `\n*-* ${meal.trim()}`;
+            }
+            return `*-* ${meal.trim()}`;
+          })
+          .join("\n");
+      };
+
+      const messageParsed = ruMenu
+        .map((day: any) => {
+          return `\n*${day.weekDay}* \n
+      *_${day.breakfast.title}_*
+      ${parseMenu(day.breakfast.menu)} \n
+      *_${day.lunch.title}_*
+      ${parseMenu(day.lunch.menu)} \n
+      *_${day.dinner.title}_*
+      ${parseMenu(day.dinner.menu)}\n`;
+        })
+        .join("\n");
+
+      const message = `
+        Cardápio RU Centro Politécnico \n 
+        ${messageParsed}`;
+
+      let payload: object = { text_reply: message };
+
+      params!.specific = payload;
+
+      let command_result: any = new SendReplyCommand().Exec_command(0, params);
+    } catch (error) {
+      console.log("Erro em Ru: ", error);
+    }
+  }
+}
--- cassiohcore/Controller/CommandsController.ts
+++ cassiohcore/Controller/CommandsController.ts
@@ -7,63 +7,80 @@ import { SpottedCommand } from "../Commands/Spotted";
 import { RollDiceCommand } from "../Commands/Roll";
 import { AdminShellCommand } from "../Commands/AdminShell";
 import { MoneyToRuCommand } from "../Commands/MoneyToRu";
+import { RuMenuCommand } from "../Commands/RuMenu";
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 class CommandsController {
-    constructor(public Commands_list: Array<CommandModel>) { }
+  constructor(public Commands_list: Array<CommandModel>) {}
 
-    public async Run_command(user_access_level: number, params: params_to_command): Promise<void> {
-        let trigger_key: string = params.command_key!;
-        let command: CommandModel = _.find(this.Commands_list, function (el: CommandModel) {
-            return el.key == trigger_key;
-        });
+  public async Run_command(
+    user_access_level: number,
+    params: params_to_command
+  ): Promise<void> {
+    let trigger_key: string = params.command_key!;
+    let command: CommandModel = _.find(
+      this.Commands_list,
+      function (el: CommandModel) {
+        return el.key == trigger_key;
+      }
+    );
 
-        if (command) {
-            let command_result = await command.Exec_command(user_access_level, params);
-            command_result ? console.log("Executado com sucesso") : console.log("Algo de errado aconteceu");
-            return;
-        }
-        console.log("Comando não encontrado");
+    if (command) {
+      let command_result = await command.Exec_command(
+        user_access_level,
+        params
+      );
+      command_result
+        ? console.log("Executado com sucesso")
+        : console.log("Algo de errado aconteceu");
+      return;
     }
+    console.log("Comando não encontrado");
+  }
 }
 
 export class CommandsControllerService {
-    private static _commands_array: Array<CommandModel>;
-    private _command_hello: CommandModel;
-    private _command_everyone: EveryoneCommand;
-    private _command_help: HelpCommand;
-    private _command_spotted: SpottedCommand;
-    private _command_roll: RollDiceCommand;
-    private _command_shell: AdminShellCommand;
-    private _command_moneyToRu: MoneyToRuCommand;
+  private static _commands_array: Array<CommandModel>;
+  private _command_hello: CommandModel;
+  private _command_everyone: EveryoneCommand;
+  private _command_help: HelpCommand;
+  private _command_spotted: SpottedCommand;
+  private _command_roll: RollDiceCommand;
+  private _command_shell: AdminShellCommand;
+  private _command_moneyToRu: MoneyToRuCommand;
+  private _command_ruMenu: RuMenuCommand;
 
-    public Command_service: CommandsController;
+  public Command_service: CommandsController;
 
-    constructor() {
-        CommandsControllerService._commands_array = [];
+  constructor() {
+    CommandsControllerService._commands_array = [];
 
-        this._command_hello = new HelloWorldCommand();
-        this._command_everyone = new EveryoneCommand();
-        this._command_help = new HelpCommand();
-        this._command_spotted = new SpottedCommand();
-        this._command_roll = new RollDiceCommand();
-        this._command_shell = new AdminShellCommand();
-        this._command_moneyToRu = new MoneyToRuCommand();
+    this._command_hello = new HelloWorldCommand();
+    this._command_everyone = new EveryoneCommand();
+    this._command_help = new HelpCommand();
+    this._command_spotted = new SpottedCommand();
+    this._command_roll = new RollDiceCommand();
+    this._command_shell = new AdminShellCommand();
+    this._command_moneyToRu = new MoneyToRuCommand();
+    this._command_ruMenu = new RuMenuCommand();
 
-        CommandsControllerService._commands_array.push(this._command_hello);
-        CommandsControllerService._commands_array.push(this._command_everyone);
-        CommandsControllerService._commands_array.push(this._command_help);
-        CommandsControllerService._commands_array.push(this._command_spotted);
-        CommandsControllerService._commands_array.push(this._command_roll);
-        CommandsControllerService._commands_array.push(this._command_shell);
-        CommandsControllerService._commands_array.push(this._command_moneyToRu);
+    CommandsControllerService._commands_array.push(this._command_hello);
+    CommandsControllerService._commands_array.push(this._command_everyone);
+    CommandsControllerService._commands_array.push(this._command_help);
+    CommandsControllerService._commands_array.push(this._command_spotted);
+    CommandsControllerService._commands_array.push(this._command_roll);
+    CommandsControllerService._commands_array.push(this._command_shell);
+    CommandsControllerService._commands_array.push(this._command_moneyToRu);
+    CommandsControllerService._commands_array.push(this._command_ruMenu);
 
+    this.Command_service = new CommandsController(
+      CommandsControllerService._commands_array
+    );
+  }
 
-        this.Command_service = new CommandsController(CommandsControllerService._commands_array);
-    }
-
-    static Get_commands_list(): any {
-        return CommandsControllerService._commands_array;
-    }
+  static Get_commands_list(): any {
+    return CommandsControllerService._commands_array;
+  }
 }
+
