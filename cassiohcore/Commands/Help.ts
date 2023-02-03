1f8a00710bd3365c80bfa508b574aad0d10838ee
AlmiranteFuchs <58078331+AlmiranteFuchs@users.noreply.github.com>
2023-02-03 01:10:56 -0300

    fix: help.ts

--- cassiohcore/Commands/Help.ts
+++ cassiohcore/Commands/Help.ts
@@ -47,8 +47,8 @@ export class HelpCommand extends CommandModel {
                 rowss.map((row: any) =>
                     "_*" + row.title + (row.alias != undefined ? (" ou /" + row.alias) : "") + "*_" +
                     (row.useLimit > 0 ? (" *Usos:* " + row.useLimit) : "") + "\n" +
-                    " - " + row.description + "\n").join("\n"), params)+
-                    "\n\n_Criado por Fuchs e colaboradores 💓, versão v3_\n_https://github.com/AlmiranteFuchs/Casioh_";
+                    " - " + row.description + "\n").join("\n") +
+                "\n\n_Criado por Fuchs e colaboradores 💓, versão v3_\n_https://github.com/AlmiranteFuchs/Casioh_", params);
         } catch (error) {
             console.log("Erro em Help: ", error);
         }
